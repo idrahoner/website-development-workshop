6d7c3cae3840f952605ba05a95653b5e663cdb62
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-05-01 10:22:32 +0300

    Add slider and lightgox

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,38 @@
 "use strict";
 const title = document.querySelector(".cats-title");
-console.log(title);
-title.classList.remove("cats-title");
+const logotype = document.querySelector(".site-logo img");
+const body = document.querySelector("body");
+function onLogoClick() {
+    logotype.classList.toggle("is-hidden");
+}
+title.addEventListener("click", onLogoClick);
+const catPhoto = document.querySelector(".cat-photo");
+function onPhotoClick() {
+    catPhoto.src = "https://picsum.photos/200/300";
+    catPhoto.width = 296;
+    catPhoto.height = 370;
+}
+catPhoto.addEventListener("click", onPhotoClick);
+const slider = document.querySelector(".hero-list");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const mySiema = new Siema({
+    selector: slider,
+    loop: true,
+    duration: 1000,
+    easing: "cubic-bezier(.17,.67,.32,1.34)",
+});
+function onNextClick() {
+    mySiema.next();
+}
+function onPrevClick() {
+    mySiema.prev();
+}
+next.addEventListener("click", onNextClick);
+prev.addEventListener("click", onPrevClick);
+const lightbox = new SimpleLightbox(".gallery a", {
+    closeText: "&#128514;",
+    fadeSpeed: 1000,
+});
+AOS.init();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,50 @@
 const title = document.querySelector(".cats-title")! as HTMLTitleElement;
+const logotype = document.querySelector(".site-logo img")!;
+const body = document.querySelector("body")!;
 
-console.log(title);
+function onLogoClick() {
+  logotype.classList.toggle("is-hidden");
+}
 
-// title.textContent = "Hello, world!";
-// title.style.color = "tomato";
-// title.style.fontSize = "50px";
-// title.style.cssText = "background-color: teal; color: tomato; font-size: 50px;";
+title.addEventListener("click", onLogoClick);
 
-title.classList.remove("cats-title");
+const catPhoto = document.querySelector(".cat-photo")! as HTMLImageElement;
+
+function onPhotoClick() {
+  catPhoto.src = "https://picsum.photos/200/300";
+  catPhoto.width = 296;
+  catPhoto.height = 370;
+}
+catPhoto.addEventListener("click", onPhotoClick);
+
+const slider = document.querySelector(".hero-list")!;
+const next = document.querySelector(".next")!;
+const prev = document.querySelector(".prev")!;
+
+// @ts-ignore
+const mySiema = new Siema({
+  selector: slider,
+  loop: true,
+  duration: 1000,
+  easing: "cubic-bezier(.17,.67,.32,1.34)",
+});
+
+function onNextClick() {
+  mySiema.next();
+}
+
+function onPrevClick() {
+  mySiema.prev();
+}
+
+next.addEventListener("click", onNextClick);
+prev.addEventListener("click", onPrevClick);
+
+// @ts-ignore
+const lightbox = new SimpleLightbox(".gallery a", {
+  closeText: "&#128514;",
+  fadeSpeed: 1000,
+});
+
+// @ts-ignore
+AOS.init();
